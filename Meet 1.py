d552ff8f392b41f0d40209c83fbb294963bab1aa
Wisnu_Alprog-C <wisnutazridhuwisnu@gmail.com>
2026-04-21 14:01:19 +0800

    Add files via upload

---
+++ Meet 1.py
@@ -0,0 +1,9 @@
+nama = input("masukkan nama anda :")
+if nama=="wismu" :
+    print("selamat datang wisnu")
+elif nama=="wisnu tazridhu" : 
+    print("anda bukan mahasiswa undikma")
+    print("anda tidak terdaftar")
+else :
+    print("nim saya adalah:25241005")
+print("program selesai")
